102262d331e9128af5c349da1e2d5556cad82fa2
Suman Balayar <076bei042.suman@pcampus.edu.np>
2023-08-05 14:04:14 +0545

    fix frontend

--- src/Components/SensorCard.js
+++ src/Components/SensorCard.js
@@ -4,7 +4,7 @@ import Card from "./Card";
 import { WiHumidity } from "react-icons/wi";
 import { TiWeatherPartlySunny } from "react-icons/ti";
 import { FaCity } from "react-icons/fa";
-import { BsMap } from "react-icons/bs";
+import { BsMap,BsFillSunFill } from "react-icons/bs";
 import { FiSettings } from "react-icons/fi";
 
 const WeatherCard = () => {
@@ -39,20 +39,22 @@ const WeatherCard = () => {
       </div>
       <div className="h-full min-h-screen text-white w-full">
         <SearchBar />
-        <div className="flex flex-col items-center justify-center ">
-          <div className="flex mr-[14%]">
+        <div className="flex items-center justify-center space-x-8 ">
+          <div className="flex flex-col ">
             <div className="flex flex-col items-start justify-start">
               <span className="font-bold text-3xl">Madrid</span>
               <h3 className="font-medium text-slate-500">chance of rain:3%</h3>
-              <span className="py-8 text-5xl font-serif">45</span>
+              <span className="py-8 text-5xl font-serif">45&deg;</span>
             </div>
-            <div>Image Component</div>
-          </div>
-          <div className="flex items-center justify-center space-x-4">
             <div className="flex flex-col space-y-6">
               {datalist.slice(0, 4).map((data, index) => (
                 <Card key={index} data={data} />
               ))}
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center ">
+          <div className="flex items-center justify-center py-5">
+              <BsFillSunFill color="orange" fontSize="8.2rem"/>
             </div>
             <div className="flex flex-col space-y-6">
               {datalist.slice(4, 8).map((data, index) => (
